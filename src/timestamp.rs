4c1c60b3c0020d9f61a1df028102244bda22d300
Philippe Loctaux <p@philippeloctaux.com>
2024-12-24 11:00:36 +0100

    timestamp: testing deserialize

--- src/timestamp.rs
+++ src/timestamp.rs
@@ -61,3 +61,51 @@ pub struct TimestampError {
 pub(crate) fn parse_timestamp(timestamp: i64) -> Result<Timestamp, TimestampError> {
     Timestamp::from_second(timestamp).context(TimestampSnafu { timestamp })
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn deserialize_required_timestamp_string() {
+        #[derive(Deserialize)]
+        struct Test {
+            #[serde(rename = "timestamp")]
+            #[serde(with = "crate::timestamp::required")]
+            timestamp: Timestamp,
+        }
+
+        let json = r#"{"timestamp": "toto mange du gateau"}"#;
+        let test: Result<Test, _> = serde_json::from_str(json);
+        assert!(test.is_err());
+
+        let json = r#"{"timestamp": "2024-12-24T10:44:58Z"}"#;
+        let test: Result<Test, _> = serde_json::from_str(json);
+        assert_eq!(test.unwrap().timestamp.as_second(), 1735037098);
+    }
+
+    #[test]
+    fn deserialize_optional_timestamp_string() {
+        #[derive(Deserialize)]
+        struct Test {
+            #[serde(rename = "timestamp")]
+            #[serde(with = "crate::timestamp::optional")]
+            timestamp: Option<Timestamp>,
+        }
+
+        let json = r#"{"timestamp": null}"#;
+        let test: Result<Test, _> = serde_json::from_str(json);
+        assert!(test.is_ok());
+
+        let json = r#"{"timestamp": "toto mange du gateau"}"#;
+        let test: Result<Test, _> = serde_json::from_str(json);
+        assert!(test.is_err());
+
+        let json = r#"{"timestamp": "2024-12-24T10:44:58Z"}"#;
+        let test: Result<Test, _> = serde_json::from_str(json);
+        assert_eq!(
+            test.unwrap().timestamp.map(|t| t.as_second()),
+            Some(1735037098)
+        );
+    }
+}
